87a5e31ed02e92cc6c658d2ca7a66d371f000818
Iain <shadybean@gmail.com>
2017-10-28 15:17:15 +0100

    Bad things - removed server

--- server/rest/msssql.js
+++ server/rest/msssql.js
@@ -1,9 +1,23 @@
 const sql = require('mssql')
 
+var config = {
+    user: 'MPP1', 
+    password: 'K.F3;gGAJ)+4xUbk',
+    server: 'ec2-52-211-119-222.eu-west-1.compute.amazonaws.com',
+    database: 'MPP_TEAM_DEATH'
+}
+
 function getDeceased () {
    try {
-       const pool = await sql.connect('mssql://MPP1:K.F3;gGAJ)+4xUbk@ec2-52-211-119-222.eu-west-1.compute.amazonaws.com/MPP_TEAM_DEATH')
-       const result = await sql.query`select * from deceased`
+       var connection = new sql.Connecton(config);
+       var request = new sql.Request(connection);
+       request.query("select * from deceased", function (err, recordset) {
+           if (err) console.log(err);
+           else {
+               console.log(recordset);
+               return(recordset);
+           }
+       })
        return result;
    } catch (err) {
        // ... error checks
