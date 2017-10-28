b830765850bb4549aedc546ebee64b292824d5cf
Iain <shadybean@gmail.com>
2017-10-28 22:31:38 +0100

    Added API verbs for recipients

--- rest/api.js
+++ rest/api.js
@@ -4,6 +4,7 @@ var express = require('express');
 //API implementations
 var login = require('./Login');
 var deceased = require('./Deceased');
+var recipients = require('./Recipient');
 
 // express router
 var router = express.Router();
@@ -48,6 +49,36 @@ module.exports = function() {
         deceased.patchSetInterval((e, r) => {
             res.json ( { error:e, set:r});
         }, req.params.id, req.body.frequency);
+    });
+
+    router.delete('/deceased/:id', function (req, res) {
+        deceased.deleteDeceased((e, r) => {
+            res.json ( {error: e, set: r});
+        }, req.params.id);
+    });
+
+    router.get('/recipients/:id', function (req, res) {
+        recipients.getRecipient((e, r) => {
+            res.json ( {error: e, set: r});
+        }, req.params.id);
+    });
+
+    router.post('/recipients', function (req, res) {
+        recipients.postRecipient((e,r) => {
+            res.json ( {error: e, set: r});
+        }, req.body.firstName, req.body.lastName, req.body.recipientNickName, req.body.phone, req.body.email, req.body.twitter);
+    });
+
+    router.patch('/recipients/:id' = function (req, res) {
+        recipients.patchRecipient((e,r) => {
+            res.json ( {error: e, set: r});
+        }, req.params.id, req.body.firstName, req.body.lastName, req.body.recipientNickName, req.body.phone, req.body.email, req.body.twitter);
+    });
+
+    router.delete('/recipients/:id' = function (req, res) {
+        recipients.deleteRecipient((e,r) => {
+            res.json ( {error: e, set: r});
+        }, req.params.id);
     });
 
     return router;
