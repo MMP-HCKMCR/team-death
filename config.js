82f2736b43cab31cf767a675c0178ff63ddb71f5
Matthew Kelly <badgerati42@gmail.com>
2017-10-28 16:16:02 +0100

    Adds clockwork api

--- config.js
+++ config.js
@@ -10,5 +10,9 @@ module.exports = {
         password: 'K.F3;gGAJ)+4xUbk',
         server: 'ec2-52-211-119-222.eu-west-1.compute.amazonaws.com',
         database: 'MPP_TEAM_DEATH'
+    },
+
+    clockwork: {
+        key: 'dce76bb6130ac3f7c01588489ed8f35b01eaa55a'
     }
 }
