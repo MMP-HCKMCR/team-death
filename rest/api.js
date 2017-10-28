200e707b1528cdbc589a0aa9d2b8d3f555d79bfa
Iain <shadybean@gmail.com>
2017-10-28 20:31:31 +0100

    Added login

--- rest/api.js
+++ rest/api.js
@@ -2,6 +2,7 @@
 var express = require('express');
 
 //API implementations
+var login = require('./Login');
 var deceased = require('./Deceased');
 
 // express router
@@ -9,6 +10,12 @@ var router = express.Router();
 
 module.exports = function() {
 
+    router.post('/login', function (req, res) {
+        login.login((e,r) => {
+            res.json( {error: e, set: r});
+        }, req);
+    });
+    
     router.get('/', function (req, res) {
         res.send('Hello World!');
     });
@@ -20,7 +27,21 @@ module.exports = function() {
     });
 
     router.post('/deceased', function (req, res) {
-        deceased.postDeceased()
+        deceased.postDeceased((e,r) => {
+            res.json( {error: e, set: r});
+        }, req.body.firstName, req.body.lastName, req.body.email, req.body.phone);
+    });
+
+    router.patch('/deceased/:id/hasDied', function (req, res) {
+        deceased.patchSetDeceased((e,r) => {
+            res.json( {error: e, set: r});
+        }, id, req.body.deceasedDate);
+    });
+
+    router.patch('/deceased/:id/checkin', function (req, res) {
+        deceased.patchSetCheckIn((e, r) => {
+            res.json( {error: e, set: r});
+        }, id);
     });
 
     return router;
