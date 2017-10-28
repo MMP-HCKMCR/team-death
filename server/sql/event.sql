f00f14bc922e9ce9a3ec7fe5b83e4bef0a6630ed
John <github@armyofdarkness.co.uk>
2017-10-28 15:57:21 +0100

    more DB fixes

--- server/sql/event.sql
+++ server/sql/event.sql
@@ -1,16 +1,12 @@
 CREATE TABLE event (
 	eventId INT IDENTITY(1,1) PRIMARY KEY,
 	eventDate DATETIME,
-	eventTypeId INT,
-	recipientId INT,
-	deceasedId INT,
+	eventTypeId INT NOT NULL FOREIGN KEY REFERENCES eventType(eventTypeId),
+	recipientId INT NOT NULL FOREIGN KEY REFERENCES recipient(recipientId),
+	deceasedId INT NOT NULL FOREIGN KEY REFERENCES deceased(deceasedId),
 	annualRepeat BIT DEFAULT 0,
-	messageId INT,
+	messageId INT NOT NULL FOREIGN KEY REFERENCES message(messageId),
 	SMS BIT DEFAULT 0,
 	email BIT DEFAULT 0,
 	twitter BIT DEFAULT 0,
-	FK_eventTypeId INT FOREIGN KEY REFERENCES eventType(eventTypeId),
-	FK_recipientId INT FOREIGN KEY REFERENCES recipient(recipientId),
-	FK_deceasedId INT FOREIGN KEY REFERENCES deceased(deceasedId),
-	FK_messageId INT FOREIGN KEY REFERENCES message(messageId)
 );
