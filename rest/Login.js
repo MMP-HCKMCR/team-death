c0966c4d2584e33c5f961e8e25aaf47b2c5510a4
Iain <shadybean@gmail.com>
2017-10-29 06:36:46 +0000

    Changed login logic to set last login time
    and reset the notDeadYet alert date

--- rest/Login.js
+++ rest/Login.js
@@ -1,10 +1,12 @@
 var sqlConn = require('./SqlConn');
+var sqlDate = require('./msDate');
 
 exports.login = function(cb, email) {
+    let date = sqlDate.getDate();
     try {
         sqlConn.getSqlRequest((err, sqlRequest) => {
             if (err) {cb(err); return;}
-            sqlRequest.query(`SELECT deceasedId FROM Deceased WHERE email = '${email}'`, (err, results) => {
+            sqlRequest.query(`UPDATE deceased SET LastLoginDate = '${date}', notDeadYet = null OUTPUT inserted.deceasedId WHERE email = '${email}'`, (err, results) => {
                 if (err)  { cb(err); return; }
                 cb(null, results);
             });
